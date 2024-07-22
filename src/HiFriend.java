586217a28733a733b86d73ecc78242eb2003cd40
T4nGe4nT <nichholcomb@gmail.com>
2024-07-22 16:05:44 -0400

    Initial commit

---
+++ src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String a = "Hi";
+        String b = " Daffy Duck!";
+        System.out.println(a + b);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        MyNameIs nm = new MyNameIs();
+        Vacation vc = new Vacation();
+        HiFriend hf = new HiFriend();
+
+        System.out.println(nm);
+        System.out.println(vc);
+        System.out.println(hf);
+
+    }
+}
